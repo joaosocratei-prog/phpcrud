5634bc31c422c7e053b1b417cd12f90424bd7952
SOCO-PROG <joaosocratei@gmail.com>
2026-09-17 05:23:33 -0700

    Modify index.php for title and redirection
    Updated the title and added Google Search Console verification. Redirects to login page with a message.

--- index.php
+++ index.php
@@ -1,45 +1,34 @@
+```php
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        .name{
-            padding-top: 90px;
-            margin-left: 210px;
-            color:blanchedalmond;
-            font-size: 40px;
 
-        
-    
-        }
-        body{
-            background-image: url(figure.png);
-            background-repeat: no-repeat;
-            background-size: cover;
-            justify-content: center;
-            
-        }
-        img{
-            height: 70px;
-            width: 70px;
-            border-radius: 95%;
-            border-color: azure;
+    <title>SoCo Messenger</title>
 
-        }
-        a{
-            font-size: 25px;
-            margin-left: 500px;
-            text-decoration: none;
-        }
-    
-
-    </style>
+    <!-- Google Search Console Verification -->
+    <meta name="google-site-verification"
+          content="2wkfXg2R1hqd2J1n1c20r_fEWMGaTFuDGdYU5fdZTEc">
+<!-- Google Search Console Verification -->
+    <meta name="google-site-verification" content="2wkfXg2R1hqd2J1n1c20r_fEWMGaTFuDGdYU5fdZTEc">
+    <!-- Redirect to login -->
+    <meta http-equiv="refresh" content="0;url=login.php">
 </head>
-<body><div class="name">
-<h3>okey, welcome user you are allowed to use this chatdot</h3>
-    <h3>and also you should press continue to continue to your chat</h3></div>
-    <a href="chat.php">continue</a>
+
+<body>
+
+    <p>Redirecting to login...</p>
+
+    <script>
+        window.location.href = "login.php";
+    </script>
+
 </body>
-</html>     
+</html>
+```
